911310d22567a0bf6e2cb6832d37efd0a1568323
FernandaA01711870 <142357001+FernandaA01711870@users.noreply.github.com>
2023-11-19 16:14:55 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 #include<stdlib.h>
 
-#include "elementoBiblio.h"
+#include "ElementoBibliografico.h"
 #include "Usuario.h"
 #include "Prestamo.h"
 
@@ -9,22 +9,34 @@ using namespace std;
 
 int main(){
 	
-	//Objetos
-	
+	//Creacion de objetos
 	ElementoBibliografico objeto1("Libro", 1234, "Augusto", "Las mil letras", 1998, true);
-	cout<<"Tipo de material: "<<objeto1.getTipo()<<", Codigo: "<<objeto1.getCodigo()<<", Autor: "<<objeto1.getAutor()<<
-	", Titulo: "<<objeto1.getTitulo()<<", Anio: "<<objeto1.getAnio()<<", Status: ";objeto1.verStatus();
 	
-	Usuario persona1("nombre1", "apellido1", 15, "@correo", 4415247854, 2);
-	cout<<endl<<"Nombre: "<<persona1.getNombre()<<", Apellido: "<<persona1.getApellido()<<", Edad: "<<persona1.getEdad()<<
-	", Correo: "<<persona1.getCorreo()<<", Numero Telefonico: "<<persona1.getTelefono()<<", Numero de sanciones: "<<persona1.getSancion()<<endl<<endl;
+	Prestamo prestamo1(objeto1, 1, "1 de Marzo", "10 de Marzo");
 	
-	Prestamo prestamo1(1234, 1, "1 de Marzo", "10 de Marzo");
-	cout<<"El libro con codigo: "<<prestamo1.getCodigo()<<" fue prestado el dia: "<<prestamo1.getFechaSalida()<<
-	" y fue devuleto el dia: "<<prestamo1.getFechaRegreso()<<endl<<endl;
+	Usuario persona1("Jose", "Ramirez", 15, "@correo", 4415247854,prestamo1);
 	
+	//Imprimir datos
+	cout<<"--Inventario de material--"<<endl<<endl;;
+	objeto1.imprimirDatos();
+	cout<<"El status del material es: ";objeto1.verStatus();cout<<endl;
 	
+	cout<<"--Registro de prestamos--"<<endl<<endl;;
+	prestamo1.imprimirDatos();
+	
+	cout<<"--Usuarios--"<<endl<<endl;;
+	persona1.imprimirDatos();
+	
+	//Usuario lleva y devuelve elementos
+	cout<<"--Usuarios llevando y devolviendo elementos--"<<endl<<endl;;
+	persona1.llevarElemento(objeto1);
+	
+	cout<<"El status actual del material es: ";objeto1.verStatus();cout<<endl;
+	
+	persona1.devolverElemento(objeto1);
+	
+	cout<<"El status actual del material es: ";objeto1.verStatus();
+
 	system("pause");
 	return 0;
 }
-
